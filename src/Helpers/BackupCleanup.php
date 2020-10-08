83e2c2eec889e3dee10aab7bf60d5b9cef05c171
laur sandu <Sapistudio@users.noreply.github.com>
2020-10-08 22:45:39 +0300

    Update BackupCleanup.php

--- src/Helpers/BackupCleanup.php
+++ src/Helpers/BackupCleanup.php
@@ -6,47 +6,49 @@ use Illuminate\Support\Collection;
 
 class BackupCleanup
 {
+    protected $deletedBackups = null;
     protected $newestBackup;
     protected $cleanupConfig = [
-            'keepAllBackupsForDays'         => 0,/** The number of days for which all backups must be kept. */
-            'keepDailyBackupsForDays'       => 16,/** The number of days for which all daily backups must be kept.*/
-            'keepWeeklyBackupsForWeeks'     => 8,/** The number of weeks for which all one weekly backup must be kept.*/
-            'keepMonthlyBackupsForMonths'   => 4,/** The number of months for which one monthly backup must be kept.*/
-            'keepYearlyBackupsForYears'     => 2,/** The number of years for which one yearly backup must be kept. */
-            'deleteOldestBackupsWhenUsingMoreMegabytesThan' => 5000,/** After cleaning up backups, remove the oldest backup until this number of megabytes has been reached.*/
-        ];
+        'numberOfBackupsPerPeriod'      => 0,/** The number of backups must be kept on period. */
+        'keepDailyBackupsForDays'       => 16,/** The number of days for which all daily backups must be kept.*/
+        'keepWeeklyBackupsForWeeks'     => 8,/** The number of weeks for which all one weekly backup must be kept.*/
+        'keepMonthlyBackupsForMonths'   => 4,/** The number of months for which one monthly backup must be kept.*/
+        'deleteOldestBackupsWhenUsingMoreMegabytesThan' => 5000,/** After cleaning up backups, remove the oldest backup until this number of megabytes has been reached.*/
+    ];
     
     /** BackupCleanup::deleteOldBackups()*/
     public function deleteOldBackups(BackupCollection $backups)
     {
         // Don't ever delete the newest backup.
-        $this->newestBackup = $backups->shift();
-        $dateRanges         = $this->calculateDateRanges();
-        $backupsPerPeriod   = $dateRanges->map(function (Period $period) use ($backups) {
-            return $backups->filter(function (BackupFile $backup) use ($period) {
+        $this->deletedBackups   = new BackupCollection();
+        $this->newestBackup     = $backups->shift();
+        $dateRanges             = $this->calculateDateRanges();
+        $backupsPerPeriod       = $dateRanges->map(function (Period $period) use ($backups) {
+            return $backups->filter(function (BackupFile $backup) use ($period){
                 return $backup->date()->between($period->getStartDate(),$period->getEndDate());
             });
         });
         $backupsPerPeriod['daily']      = $this->groupByDateFormat($backupsPerPeriod['daily'], 'Ymd');
         $backupsPerPeriod['weekly']     = $this->groupByDateFormat($backupsPerPeriod['weekly'], 'YW');
         $backupsPerPeriod['monthly']    = $this->groupByDateFormat($backupsPerPeriod['monthly'], 'Ym');
-        $backupsPerPeriod['yearly']     = $this->groupByDateFormat($backupsPerPeriod['yearly'], 'Y');
-        $this->removeBackupsForAllPeriodsExceptOne($backupsPerPeriod);
-        $this->removeBackupsOlderThan($dateRanges['yearly']->getEndDate(), $backups);
+        
+        $this->removeBackupsForAllPeriods($backupsPerPeriod);
+        $this->removeBackupsOlderThan($dateRanges['monthly']->getEndDate(),$backups);
         $this->removeOldestsBackupsUntilUsingMaximumStorage($backups);
+        $cleanedList = $this->deletedBackups->convertToList();
+        $this->deletedBackups->each(function (BackupFile $backup){
+            $backup->delete();
+        });
+        return $cleanedList;
     }
 
     /** BackupCleanup::calculateDateRanges()*/
     protected function calculateDateRanges()
     {
-        $daily      = new Period(
-            Carbon::now()->subDays($this->cleanupConfig['keepAllBackupsForDays']),
-            Carbon::now()->subDays($this->cleanupConfig['keepAllBackupsForDays'])->subDays($this->cleanupConfig['keepDailyBackupsForDays'])
-        );
+        $daily      = new Period(Carbon::now()->subDays(0),Carbon::now()->subDays(0)->subDays($this->cleanupConfig['keepDailyBackupsForDays']));
         $weekly     = new Period($daily->getEndDate(),$daily->getEndDate()->subWeeks($this->cleanupConfig['keepWeeklyBackupsForWeeks']));
         $monthly    = new Period($weekly->getEndDate(),$weekly->getEndDate()->subMonths($this->cleanupConfig['keepMonthlyBackupsForMonths']));
-        $yearly     = new Period($monthly->getEndDate(),$monthly->getEndDate()->subYears($this->cleanupConfig['keepYearlyBackupsForYears']));
-        return collect(compact('daily', 'weekly', 'monthly', 'yearly'));
+        return collect(compact('daily', 'weekly', 'monthly'));
     }
 
     /** BackupCleanup::groupByDateFormat()*/
@@ -57,14 +59,18 @@ class BackupCleanup
         });
     }
 
-    /** BackupCleanup::removeBackupsForAllPeriodsExceptOne()*/
-    protected function removeBackupsForAllPeriodsExceptOne($backupsPerPeriod)
+    /** BackupCleanup::removeBackupsForAllPeriods()*/
+    protected function removeBackupsForAllPeriods($backupsPerPeriod)
     {
+        $numberOfBackups = $this->cleanupConfig['numberOfBackupsPerPeriod'];
         foreach ($backupsPerPeriod as $periodName => $groupedBackupsByDateProperty) {
-            $groupedBackupsByDateProperty->each(function (Collection $group) {
-                $group->shift();
-                $group->each(function (BackupFile $backup) {
-                    $backup->delete();
+            $groupedBackupsByDateProperty->each(function (Collection $group) use ($numberOfBackups) {
+                if($numberOfBackups > 0){
+                    for($keepBackup = 0;$keepBackup < $numberOfBackups;$keepBackup++)
+                        $group->shift();
+                }
+                $group->each(function (BackupFile $backup){
+                    $this->deleteBackup($backup);
                 });
             });
         }
@@ -76,7 +82,7 @@ class BackupCleanup
         $backups->filter(function (BackupFile $backup) use ($endDate) {
             return $backup->exists() && $backup->date()->lt($endDate);
         })->each(function (BackupFile $backup) {
-            $backup->delete();
+            $this->deleteBackup($backup);
         });
     }
 
@@ -92,5 +98,10 @@ class BackupCleanup
         }
         $oldestBackup->delete();
         $this->removeOldestsBackupsUntilUsingMaximumStorage($backups);
+    }
+    
+    /** BackupCleanup::removeOldestsBackupsUntilUsingMaximumStorage()*/
+    protected function deleteBackup(BackupFile $backup){
+        $this->deletedBackups->push($backup);
     }
 }
