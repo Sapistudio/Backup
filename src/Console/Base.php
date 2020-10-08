190b2e760adbd976c70665d841fecb25cc8bb10d
laur sandu <Sapistudio@users.noreply.github.com>
2020-10-08 22:45:23 +0300

    Update Base.php

--- src/Console/Base.php
+++ src/Console/Base.php
@@ -1,6 +1,7 @@
 <?php
 namespace SapiStudio\Backup\Console;
 
+use Illuminate\Support\Str;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
@@ -15,13 +16,15 @@ use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 
 class Base extends Command
 {
-    protected static $defaultName = 'base';
+    protected static $lineLength    = 100;
+    protected static $defaultName   = 'base';
     protected static $input;
     protected static $output;
     protected static $progressBar;
     protected static $formatter;
     
-    public static function createApp(){
+    /** Base::createConsole()*/
+    public static function createConsole(){
         $command        = new static();
         $application    = new Application();
         $application->add($command);
@@ -31,6 +34,7 @@ class Base extends Command
         return new static();
     }
     
+    /** Base::execute() */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         self::$formatter = $this->getHelper('formatter');
@@ -38,66 +42,73 @@ class Base extends Command
         self::$output   = $output;
     }
     
+    /** Base::startProgressBar() */
     public function startProgressBar($total = 100){
         self::$progressBar = new ProgressBar(self::$output, $total);
-        self::$progressBar->setFormat("%message%\n%current%/%max% [%bar%] %percent:3s%% \n");
+        self::$progressBar->setFormat("%message%\n");
         self::$progressBar->start();
     }
     
+    /** Base::updateProgressBar()*/
     public function updateProgressBar($message = null){
         self::$progressBar->advance(1);
-        self::$progressBar->setMessage(self::$formatter->truncate($message, 50));
+        self::$progressBar->setMessage(self::$formatter->truncate($message, self::$lineLength));
     }
     
+    /** Base::outputTable()*/
     public function outputTable($rows)
     {
         return (!$rows) ? false : (new Table(self::$output))->setHeaders(array_keys($rows[0]))->setRows($rows)->render();
     }
     
+    /** Base::warn()*/
     public function warn($string, $verbosity = null)
     {
-        if (! self::$output->getFormatter()->hasStyle('warning')) {
+        if (!self::$output->getFormatter()->hasStyle('warning')) {
             $style = new OutputFormatterStyle('yellow');
             self::$output->getFormatter()->setStyle('warning', $style);
         }
         $this->line($string, 'warning', $verbosity);
     }
 
+    /** Base::alert() */
     public function alert($string)
     {
-        $length = Str::length(strip_tags($string)) + 12;
-
+        $length     = Str::length(strip_tags($string)) + 12;
         $this->comment(str_repeat('*', $length));
         $this->comment('*     '.$string.'     *');
         $this->comment(str_repeat('*', $length));
-
         self::$output->newLine();
     }
     
+    /** Base::error()*/
     public function error($string, $verbosity = null)
     {
         $this->line($string, 'error', $verbosity);
     }
     
+    /** Base::question() */
     public function question($string, $verbosity = null)
     {
         $this->line($string, 'question', $verbosity);
     }
     
+    /** Base::comment()*/
     public function comment($string, $verbosity = null)
     {
         $this->line($string, 'comment', $verbosity);
     }
     
+    /** Base::info()*/
     public function info($string, $verbosity = null)
     {
         $this->line($string, 'info', $verbosity);
     }
     
+    /** Base::line()*/
     public function line($string, $style = null, $verbosity = null)
     {
         $styled = $style ? "<$style>$string</$style>" : $string;
-
         self::$output->writeln($styled);
     }
 }
